9ebef23f4e180e30d8864aa80c91ac159e2d53ef
aphan079 <aphang355@gmail.com>
2024-05-19 04:32:20 -0700

    added basic user input/output stuff to match the trace

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <random>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <iomanip>
 
 using namespace std;
 
@@ -12,12 +16,55 @@ double evaluation() {
     return dis(gen);
 }
 
+void forwardSelection(int features)
+{
+
+}
 int main() {
     
-    for(int i = 0; i < 5; i++){
-        double rand = evaluation();
-        cout << rand << endl;
+    std::vector<double> accuracy;
+
+    system("cls");
+    int userInput; 
+    cout << "Welcome to the Feature Selection Algorithm \n";
+    cout << "Please enter total number of features: ";
+    cin >> userInput;
+    cout << " \n";
+
+
+    int userChoice;
+    cout << "Type the number of the algorithm you want to run(1-3): \n";
+    cout << "1. Forward Selection \n";
+    cout << "2. Backward Elimination \n";
+    cout << "3. Special Alogrithm \n";
+    do{
+        cin >> userChoice;
+        if(cin.fail()){
+            
+            cin.clear();
+            cin.ignore(10000 , '\n');
+            
+        }
+        cout << "please choose a number between 1 and 3: ";
+
+    } while(userChoice != 1 && userChoice !=2 && userChoice != 3);
+    cout << " \n";
+
+    if(userChoice == 1){
+        forwardSelection(userInput);
     }
+    else if(userChoice == 2){
+
+    }
+    else{
+
+    }
+
+    // for(int i = 0; i < 5; i++){
+    //     double rand = evaluation();
+    //     accuracy.push_back(rand);
+    //     cout << rand << endl;
+    // }
 
     return 0;
 }
