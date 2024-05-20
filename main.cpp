2bc218fa8ebe9bf33b2f09e979723e1985dcd070
Brandon Shin <bmshin02@gmail.com>
2024-05-19 23:45:28 -0700

    backwardElimation setup

--- main.cpp
+++ main.cpp
@@ -92,6 +92,86 @@ void forwardSelection(int features)
     }
     cout << "}, which has an accuracy of " << fixed << setprecision(1) << best_overall_accuracy << "%\n\n";
 }
+
+void backwardElimination(int features)
+{
+    set<int> selected_features; //used set but could probably change it to vector if needed, uses begin() and end()
+    double best_overall_accuracy = 0.0; //keeps track of highest accuracy %
+    set<int> best_overall_features; //keeps track of best feature set {}
+
+    cout << "Using no features and \"random\" evaluation, I get an accuracy of: "; //basic output stuff for trace
+    cout << fixed << setprecision(1) << evaluation() << "%\n\nBeginning search.\n\n";
+
+    for (int i = 0; i < features; i++) //loops through the features the user Inputes
+    {
+        set<int> current_best_features = selected_features;
+        double current_best_accuracy = 0.0;
+        int feature;
+
+        for (feature = 1; feature <= features; feature++) //finds the best feature set
+        {
+            if (selected_features.find(feature) == selected_features.end()) //makes sure there arent duplicates in selected_features
+            {
+                set<int> new_features = selected_features; //creates new feature set with the new feature. Ex.) {1} -> {1,3}
+                new_features.insert(feature);
+                
+                double accuracy = evaluation(); //doing the random accuracy
+                cout << "\t Using feature(s) {";
+                for (auto iterate = new_features.begin(); iterate != new_features.end(); iterate++) //auto automatically finds the type
+                {
+                    cout << *iterate; //prints the set ex.) {1}, {2}
+                    if (next(iterate) != new_features.end()) cout << ","; //checks to see if there are more elements to print
+                }
+                cout << "} accuracy is " << fixed << setprecision(1) << accuracy << "%\n";
+
+                if (accuracy > current_best_accuracy) //updates the current best accuracy
+                {
+                    current_best_accuracy = accuracy;
+                    current_best_features = new_features;
+                }
+            }
+        }
+
+        selected_features = current_best_features; //keeps track of the iterated best feature set
+
+        if (current_best_accuracy > best_overall_accuracy) //updates the overall highest accuracy
+        {
+            best_overall_accuracy = current_best_accuracy;
+            best_overall_features = selected_features;
+        }
+
+        cout << "\nFeature set {";
+        for (auto iterate = selected_features.begin(); iterate != selected_features.end(); iterate++) 
+        {
+            cout << *iterate; //outputs the current set 
+            if (next(iterate) != selected_features.end()) cout << ",";
+        }
+
+        cout << "} was best, accuracy is " ;
+        cout << fixed << setprecision(1) << current_best_accuracy << "%"; //outputs the above sets' accuracy percentage
+
+        if(current_best_accuracy < best_overall_accuracy) //conditional print statements for if there is an accuracy decrease. mostly just to match the trace formatting
+        {
+            cout << " \n(Warning, Accuracy has decreased!)\n\n";
+        } 
+        else 
+        {
+            cout << "\n\n";
+        }
+        
+    }
+    
+    cout << "\nFinished search!! The best feature subset is {";
+    for (auto iterate = best_overall_features.begin(); iterate != best_overall_features.end(); iterate++) //prints the best feature set
+    { 
+        cout << *iterate;
+        if (next(iterate) != best_overall_features.end()) cout << ",";
+    }
+    cout << "}, which has an accuracy of " << fixed << setprecision(1) << best_overall_accuracy << "%\n\n";
+}
+
+
+
 int main() {
     
     std::vector<double> accuracy;
